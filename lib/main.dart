389e39a983fe76094899d4be2221bc43a542b9e0
sharansingh00002 <sharansingh00002@gmail.com>
2019-04-19 19:45:55 +0530

    code refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ class DrawApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Draw(),
     );
   }
