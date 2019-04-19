4ae8e4ddf4af911d95f6f9f7d30e4b2649c81c4b
sharansingh00002 <sharansingh00002@gmail.com>
2019-04-19 18:51:05 +0530

    color picker added

--- lib/draw_screen.dart
+++ lib/draw_screen.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 
 class Draw extends StatefulWidget {
   @override
@@ -10,6 +11,7 @@ class Draw extends StatefulWidget {
 
 class _DrawState extends State<Draw> {
   Color selectedColor = Colors.black;
+  Color pickerColor = Colors.black;
   double strokeWidth = 3.0;
   List<DrawingPoints> points = List();
   bool showBottomList = false;
@@ -25,124 +27,122 @@ class _DrawState extends State<Draw> {
   ];
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        bottomNavigationBar: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-              padding: const EdgeInsets.only(left: 8.0, right: 8.0),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(50.0),
-                  color: Colors.greenAccent),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        IconButton(
-                            icon: Icon(Icons.album),
-                            onPressed: () {
-                              setState(() {
-                                if (selectedMode == SelectedMode.StrokeWidth)
-                                  showBottomList = !showBottomList;
-                                selectedMode = SelectedMode.StrokeWidth;
-                              });
-                            }),
-                        IconButton(
-                            icon: Icon(Icons.opacity),
-                            onPressed: () {
-                              setState(() {
-                                if (selectedMode == SelectedMode.Opacity)
-                                  showBottomList = !showBottomList;
-                                selectedMode = SelectedMode.Opacity;
-                              });
-                            }),
-                        IconButton(
-                            icon: Icon(Icons.color_lens),
-                            onPressed: () {
-                              setState(() {
-                                if (selectedMode == SelectedMode.Color)
-                                  showBottomList = !showBottomList;
-                                selectedMode = SelectedMode.Color;
-                              });
-                            }),
-                        IconButton(
-                            icon: Icon(Icons.clear),
-                            onPressed: () {
-                              setState(() {
-                                showBottomList = false;
-                                points.clear();
-                              });
-                            }),
-                      ],
-                    ),
-                    Visibility(
-                      child: (selectedMode == SelectedMode.Color)
-                          ? Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: getColorList(),
-                      )
-                          : Slider(
-                          value: (selectedMode == SelectedMode.StrokeWidth)
-                              ? strokeWidth
-                              : opacity,
-                          max: (selectedMode == SelectedMode.StrokeWidth)
-                              ? 50.0
-                              : 1.0,
-                          min: 0.0,
-                          onChanged: (val) {
+    return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+            padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(50.0),
+                color: Colors.greenAccent),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                          icon: Icon(Icons.album),
+                          onPressed: () {
                             setState(() {
                               if (selectedMode == SelectedMode.StrokeWidth)
-                                strokeWidth = val;
-                              else
-                                opacity = val;
+                                showBottomList = !showBottomList;
+                              selectedMode = SelectedMode.StrokeWidth;
                             });
                           }),
-                      visible: showBottomList,
-                    ),
-                  ],
-                ),
-              )),
-        ),
-        body: GestureDetector(
-          onPanUpdate: (details) {
-            setState(() {
-              RenderBox renderBox = context.findRenderObject();
-              points.add(DrawingPoints(
-                  points: renderBox.globalToLocal(details.globalPosition),
-                  paint: Paint()
-                    ..strokeCap = strokeCap
-                    ..isAntiAlias = true
-                    ..color = selectedColor.withOpacity(opacity)
-                    ..strokeWidth = strokeWidth));
-            });
-          },
-          onPanStart: (details) {
-            setState(() {
-              RenderBox renderBox = context.findRenderObject();
-              points.add(DrawingPoints(
-                  points: renderBox.globalToLocal(details.globalPosition),
-                  paint: Paint()
-                    ..strokeCap = strokeCap
-                    ..isAntiAlias = true
-                    ..color = selectedColor.withOpacity(opacity)
-                    ..strokeWidth = strokeWidth));
-            });
-          },
-          onPanEnd: (details) {
-            setState(() {
-              points.add(null);
-            });
-            setState(() {});
-          },
-          child: CustomPaint(
-            size: Size.infinite,
-            painter: DrawingPainter(
-              pointsList: points,
-            ),
+                      IconButton(
+                          icon: Icon(Icons.opacity),
+                          onPressed: () {
+                            setState(() {
+                              if (selectedMode == SelectedMode.Opacity)
+                                showBottomList = !showBottomList;
+                              selectedMode = SelectedMode.Opacity;
+                            });
+                          }),
+                      IconButton(
+                          icon: Icon(Icons.color_lens),
+                          onPressed: () {
+                            setState(() {
+                              if (selectedMode == SelectedMode.Color)
+                                showBottomList = !showBottomList;
+                              selectedMode = SelectedMode.Color;
+                            });
+                          }),
+                      IconButton(
+                          icon: Icon(Icons.clear),
+                          onPressed: () {
+                            setState(() {
+                              showBottomList = false;
+                              points.clear();
+                            });
+                          }),
+                    ],
+                  ),
+                  Visibility(
+                    child: (selectedMode == SelectedMode.Color)
+                        ? Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: getColorList(),
+                          )
+                        : Slider(
+                            value: (selectedMode == SelectedMode.StrokeWidth)
+                                ? strokeWidth
+                                : opacity,
+                            max: (selectedMode == SelectedMode.StrokeWidth)
+                                ? 50.0
+                                : 1.0,
+                            min: 0.0,
+                            onChanged: (val) {
+                              setState(() {
+                                if (selectedMode == SelectedMode.StrokeWidth)
+                                  strokeWidth = val;
+                                else
+                                  opacity = val;
+                              });
+                            }),
+                    visible: showBottomList,
+                  ),
+                ],
+              ),
+            )),
+      ),
+      body: GestureDetector(
+        onPanUpdate: (details) {
+          setState(() {
+            RenderBox renderBox = context.findRenderObject();
+            points.add(DrawingPoints(
+                points: renderBox.globalToLocal(details.globalPosition),
+                paint: Paint()
+                  ..strokeCap = strokeCap
+                  ..isAntiAlias = true
+                  ..color = selectedColor.withOpacity(opacity)
+                  ..strokeWidth = strokeWidth));
+          });
+        },
+        onPanStart: (details) {
+          setState(() {
+            RenderBox renderBox = context.findRenderObject();
+            points.add(DrawingPoints(
+                points: renderBox.globalToLocal(details.globalPosition),
+                paint: Paint()
+                  ..strokeCap = strokeCap
+                  ..isAntiAlias = true
+                  ..color = selectedColor.withOpacity(opacity)
+                  ..strokeWidth = strokeWidth));
+          });
+        },
+        onPanEnd: (details) {
+          setState(() {
+            points.add(null);
+          });
+          setState(() {});
+        },
+        child: CustomPaint(
+          size: Size.infinite,
+          painter: DrawingPainter(
+            pointsList: points,
           ),
         ),
       ),
@@ -154,6 +154,49 @@ class _DrawState extends State<Draw> {
     for (Color color in colors) {
       listWidget.add(colorCircle(color));
     }
+    Widget colorPicker = GestureDetector(
+      onTap: () {
+        showDialog(
+          context: context,
+          child: AlertDialog(
+            title: const Text('Pick a color!'),
+            content: SingleChildScrollView(
+              child: ColorPicker(
+                pickerColor: pickerColor,
+                onColorChanged: (color) {
+                  pickerColor = color;
+                },
+                enableLabel: true,
+                pickerAreaHeightPercent: 0.8,
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                child: const Text('Save'),
+                onPressed: () {
+                  setState(() => selectedColor = pickerColor);
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          ),
+        );
+      },
+      child: ClipOval(
+        child: Container(
+          padding: const EdgeInsets.only(bottom: 16.0),
+          height: 36,
+          width: 36,
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            colors: [Colors.red, Colors.green, Colors.blue],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+        ),
+      ),
+    );
+    listWidget.add(colorPicker);
     return listWidget;
   }
 
@@ -207,4 +250,3 @@ class DrawingPoints {
 }
 
 enum SelectedMode { StrokeWidth, Opacity, Color }
-
